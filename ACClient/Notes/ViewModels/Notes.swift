8f9d3d51f3adcbd6a28e68f47a6d16f1c6176125
Victor <abysswalk3r69@gmail.com>
2025-05-17 22:55:22 +0300

    Using dynamic routes

--- ACClient/Notes/ViewModels/Notes.swift
+++ ACClient/Notes/ViewModels/Notes.swift
@@ -6,20 +6,31 @@ import Foundation
 final class Notes {
     private(set) var error: Error?
     private(set) var data: [Note] = []
-    private let http = Client()
+    private let http: Client
+    private let route: APIRoute
 
+    init(
+        authorizationData: AuthorizationData,
+        client: Client = .init()
+    ) {
+        self.route = .init(baseURL: authorizationData.joined)
+        self.http = client
+    }
+    
     func fetch() async {
         do {
-            self.data = try await http.get(.notes)
+            error = nil
+            self.data = try await http.get(route.notes)
         } catch let error {
             self.error = error
         }
     }
     
     func create(name: String) async {
+        error = nil
         do {
             let note: Note = try await http.post(
-                to: .notes,
+                to: route.notes,
                 value: [
                     "name": name
                 ]
@@ -31,10 +42,11 @@ final class Notes {
     }
     
     func delete(at offsets: IndexSet) async {
+        error = nil
         do {
             for index in offsets {
                 let note = data[index]
-                try await http.delete(.note(id: note.id))
+                try await http.delete(route.note(id: note.id))
                 data.remove(atOffsets: [index])
             }
         } catch let error {
